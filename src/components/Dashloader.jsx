72484d17ef6a552fc1eb334fd96e04eb0511f390
prakhar-8999 <prakharporwal12a@gmail.com>
2022-10-27 12:06:58 +0530

    Loader Added

---
+++ src/components/Dashloader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/dashloader.css'
+const Dashloader = () => {
+    return (
+        <div class="box">
+            <div class="b b1"></div>
+            <div class="b b2"></div>
+            <div class="b b3"></div>
+            <div class="b b4"></div>
+        </div>
+    )
+}
+
+export default Dashloader
---
+++ src/components/Frontloader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/frontloader.css'
+const Frontloader = () => {
+    return (
+        <div id="pulse-wrapper">
+            <div id="pulse">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+
+export default Frontloader
