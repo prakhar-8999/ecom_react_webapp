1acce99b7fc311b0655c13a50a9a18fb40521a45
prakhar-8999 <prakharporwal12a@gmail.com>
2022-11-15 02:11:49 +0530

    Qr updatw

--- src/static/staticdata.jsx
+++ src/static/staticdata.jsx
@@ -1,7 +1,7 @@
 const staticdata = {
     paykey: 'rzp_test_qycxQCOUz8vIWo',
     // paykey: 'rzp_live_z2yZLQV1BRgK8i',
-    baseurl: 'https://10.21.67.37:8000/'
+    baseurl: 'https://192.168.10.84:8000/'
 }
 
 export default staticdata
